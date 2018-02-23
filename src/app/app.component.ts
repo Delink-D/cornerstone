0359c8fe6ebcf73e973efccbe9816cc3bccd54f2
Delink-D <delinkdesigns@gmail.com>
2018-02-23 10:23:34 +0300

    set the app to listen to the current url, assign default images names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import {  AfterViewInit, Component, OnInit } from '@angular/core';
 import {  AfterViewInit, Component, OnInit, Input } from '@angular/core';
 import { CornerstoneService } from './services/cornerstone.service';
 
@@ -9,17 +8,27 @@ import { CornerstoneService } from './services/cornerstone.service';
 })
 export class AppComponent implements OnInit {
   @Input() imageStore: Array<string>;
+
   imageData: any;
+  imagePath: string = window.location.host + '/assets/dicom/';
+  imageHeaders: Array<string>;
 
   constructor(public csS: CornerstoneService) { }
 
   ngOnInit() {
-  	for (const i of ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18']) {
-      this.csS.fetchDicomImage(`https://corner-1.herokuapp.com/assets/dicom/CT0000${i}.dcm`)
-      .subscribe(res => this.imageData = res);
+  	// for (const i of ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18']) {
+   //    this.csS.fetchDicomImage(`http://localhost:4200/assets/dicom/CT0000${i}.dcm`)
+   //    .subscribe(res => this.imageData = res);
+   //  }
+
+   /** from local */
+    if (this.imageData === undefined) {
+      this.imageStore = ['CT000000.dcm','CT000001.dcm','CT000002.dcm','CT000003.dcm','CT000004.dcm','CT000005.dcm','CT000006.dcm','CT000007.dcm','CT000008.dcm','CT000009.dcm','CT000010.dcm','CT000011.dcm','CT000012.dcm','CT000013.dcm','CT000014.dcm','CT000015.dcm','CT000016.dcm','CT000017.dcm','CT000018.dcm'];
     }
 
    this.getImageData(this.imageStore);
+  }
+
   getImageData (imageArray: Array<string>) {
     imageArray.forEach(image => {
       this.csS.fetchDicomImage(`${this.imagePath}${image}`)
