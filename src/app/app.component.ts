3ab87436ec417200323f9e2a6bb40161dbba2661
Delink-D <delinkdesigns@gmail.com>
2018-03-03 22:24:30 +0300

    removed commented out code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,6 @@ export class AppComponent implements OnInit {
   constructor(public csS: CornerstoneService) { }
 
   ngOnInit() {
-  	// for (const i of ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18']) {
-   //    this.csS.fetchDicomImage(`http://localhost:4200/assets/dicom/CT0000${i}.dcm`)
-   //    .subscribe(res => this.imageData = res);
-   //  }
-
    /** from local */
     if (this.imageData === undefined) {
       this.imageStore = ['CT000000.dcm','CT000001.dcm','CT000002.dcm','CT000003.dcm','CT000004.dcm','CT000005.dcm','CT000006.dcm','CT000007.dcm','CT000008.dcm','CT000009.dcm','CT000010.dcm','CT000011.dcm','CT000012.dcm','CT000013.dcm','CT000014.dcm','CT000015.dcm','CT000016.dcm','CT000017.dcm','CT000018.dcm'];
